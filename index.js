9cb49bfef9a511a7156438c1bd2e148307a38688
anjasmarats <atsakunprogramming@gmail.com>
2025-10-15 18:45:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import supabase from './supabaseClient.js';
+import bcrypt from 'bcryptjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,11 +23,34 @@ app.get('/products', async (req, res) => {
 });
 
 // POST - Buat produk baru
-app.post('/products', async (req, res) => {
-  const { name, description, price } = req.body;
+app.post('/user', async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data: user, err } = await supabase
+      .from('users')
+      .select('*')
+      .eq('email', email)
+      .single();
+
+    if (err || !user) {
+      console.error("error mengecek db", error,"\n\nuser\n\n",user)
+      return res.json(500).json({
+        message: "error mengecek db"
+      });
+    }
+
+    if (user) {
+      console.error("error, user suda ada")
+      return res.json(400).json({
+        message: "user sudah ada"
+      });
+    }
+
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  
   const { data, error } = await supabase
-    .from('products')
-    .insert([{ name, description, price }])
+    .from('users')
+    .insert([{ email, password:hashedPassword }])
     .select();
 
   if (error) {
@@ -36,12 +60,12 @@ app.post('/products', async (req, res) => {
 });
 
 // PUT - Perbarui produk berdasarkan ID
-app.put('/products/:id', async (req, res) => {
+app.put('/users/:id', async (req, res) => {
   const { id } = req.params;
-  const { name, description, price } = req.body;
+  const { email, password } = req.body;
   const { data, error } = await supabase
     .from('products')
-    .update({ name, description, price })
+    .update({ email, password })
     .eq('id', id)
     .select();
 
