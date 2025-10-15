d889f68a523a042e99afca9f3bd130f13a4c4b04
anjasmarats <atsakunprogramming@gmail.com>
2025-10-15 18:45:30 +0700

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,12 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL || "https://hofqecdlhntalcupumam.supabase.co";
+const supabaseKey = process.env.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
